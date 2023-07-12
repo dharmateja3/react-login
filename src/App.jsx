13874de8ae2fb5ccae17c4ebe503de47cd10872d
Dussa Dharma Teja <dharmatejadussa@gmail.com>
2023-07-12 15:59:47 +0530

    login

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Components/Header/Header'
+import Login from './Components/Landing/Login'
+import Footer from './Components/Footer/Footer'
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Login/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
